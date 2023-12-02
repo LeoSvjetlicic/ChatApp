5545d151a94762c37b4de85c1888c658abb69548
Leo Svjetličić <leo.svjetlicic@barrage.net>
2023-12-02 19:35:29 +0100

    Add ConnectionItem card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "ls.android.chatapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ls.android.chatapp"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,11 +32,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -47,11 +52,14 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    kapt {
+        correctErrorTypes = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -66,4 +74,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+// dagger
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-compiler:2.48.1")
+
+// encrypted shared preferences
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
 }
---
+++ app/src/main/java/ls/android/chatapp/presentation/connections/components/ConnectionItem.kt
@@ -0,0 +1,84 @@
+package ls.android.chatapp.presentation.connections.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ls.android.chatapp.domain.model.Connection
+import ls.android.chatapp.presentation.ui.DarkBlue
+import ls.android.chatapp.presentation.ui.DarkGray
+import ls.android.chatapp.presentation.ui.IceBlue
+
+@Composable
+fun ConnectionItem(
+    modifier: Modifier,
+    connection: Connection,
+    onItemClick: (String) -> Unit
+) {
+    Card(
+        modifier = modifier
+            .shadow(4.dp)
+            .clickable { onItemClick(connection.id) },
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = IceBlue,
+            contentColor = DarkBlue,
+            disabledContainerColor = IceBlue,
+            disabledContentColor = DarkBlue,
+        )
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Transparent),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(2F)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(start = 14.dp)
+                        .align(Alignment.CenterStart),
+                    text = connection.name,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 18.sp,
+                    color = DarkGray,
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1F)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(bottom = 8.dp, end = 8.dp),
+                    text = "Connected for: ${connection.daysConnected} days",
+                    fontSize = 11.sp,
+                    color = DarkGray
+                )
+            }
+        }
+    }
+}
