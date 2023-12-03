42ce7d3ac03caf81d55944320a1d0205db42cb1a
Leo Svjetličić <leo.svjetlicic@barrage.net>
2023-12-03 16:53:25 +0100

    Add FAB

---
+++ app/src/main/java/ls/android/chatapp/presentation/chat/components/AddButton.kt
@@ -0,0 +1,25 @@
+package ls.android.chatapp.presentation.chat.components
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ls.android.chatapp.presentation.ui.DarkBlue
+import ls.android.chatapp.presentation.ui.IceBlue
+
+@Composable
+fun AddButton(modifier: Modifier, onClick: () -> Unit) {
+    FloatingActionButton(
+        modifier = modifier,
+        onClick = { onClick() },
+        shape = CircleShape,
+        containerColor = DarkBlue,
+        contentColor = IceBlue
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "Add button")
+    }
+}
+
--- app/src/main/java/ls/android/chatapp/presentation/ui/Theme.kt
+++ app/src/main/java/ls/android/chatapp/presentation/ui/Theme.kt
@@ -17,15 +17,15 @@ import androidx.core.view.WindowCompat
 import hr.carnet.chatap.presentation.theme.ui.Typography
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = IceBlue,
+    secondary = DarkBlue,
+    tertiary = DarkGray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = IceBlue,
+    secondary = DarkBlue,
+    tertiary = DarkGray
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
